ba96bad504df6b546f6f25c6b84ffdf8ef53c44d
Vlad Rymarchik <vladrymarchik@gmail.com>
2025-11-10 23:37:28 +0300

    note: only layout

--- src/body/Body.tsx
+++ src/body/Body.tsx
@@ -1,18 +1,24 @@
 import "./Body.css";
 import plus from "../assets/plus.svg";
+import trash from "../assets/trash.svg";
 
 interface newProps {
-  onClickPlus: () => void
+  onClickPlus: () => void;
 }
 
-export function Body({onClickPlus} : newProps) {
+export function Body({ onClickPlus }: newProps) {
   return (
     <>
+      <div className="notes">
+        <div className="firstcont">
+          <button className="submit"></button>
+          <p>NOTE #1</p>
+        </div>
+        <button className="delete"><img src={trash}></img></button>
+      </div>
+      <hr></hr> 
       <div className="body">
-        <button 
-        className="plusBtn"
-        onClick={onClickPlus}
-        >
+        <button className="plusBtn" onClick={onClickPlus}>
           <img src={plus}></img>
         </button>
       </div>
