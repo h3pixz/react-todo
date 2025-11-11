bf3c7d30635049beabe256df723b73b1f066b992
Vlad Rymarchik <vladrymarchik@gmail.com>
2025-11-11 22:38:47 +0300

    fix layout bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Header } from "./header/Header";
 import { Body } from "./body/Body";
 import { NewVote } from "./new-vote/NewVote";
+import { Note } from "./Note/Note";
 import { useState } from "react";
 
 export function App() {
@@ -10,6 +11,7 @@ export function App() {
   return (
     <>
       <Header />
+      <Note />
       <Body onClickPlus={() => setVoteOpened(true)}/>
       {voteOpened ? <NewVote onClose={() => setVoteOpened(false)}/> : null}
     </>
--- src/body/Body.tsx
+++ src/body/Body.tsx
@@ -1,6 +1,5 @@
 import "./Body.css";
 import plus from "../assets/plus.svg";
-import { Note } from "../Note/Note";
 
 interface newProps {
   onClickPlus: () => void;
@@ -9,9 +8,6 @@ interface newProps {
 export function Body({ onClickPlus }: newProps) {
   return (
     <>
-      <Note />
-      <Note />
-      <Note />
       <div className="body">
         <button className="plusBtn" onClick={onClickPlus}>
           <img src={plus}></img>
